07d663eb401f38a22f2e9cd8292fab78d18de6ab
Alexandra BENAMAR <alexandrabenamar@client-172-18-74-108.clients.u-psud.fr>
2017-04-27 15:47:01 +0200

    fonction permettant d'afficher les informations sur le projet et son utilisation

---
+++ usage.py
@@ -0,0 +1,2 @@
+def usage() :
+    print("\n\nProjet d'Alexandra BENAMAR et Robin DROIT\nCe programme permet de parser un fichier .pdb\net calcule les distances globales et locales de la proteine.\nUsage : ./main.py -pdb <nom du fichier PDB>\n")
