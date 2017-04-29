f6d43f26829b79220012d53fc4f554e7e54b12f6
Alexandra BENAMAR <alexandra.benamar#yahoo.fr>
2017-04-29 12:28:18 +0200

    suppression de fichiers

--- usage.py
+++
@@ -1,2 +0,0 @@
-def usage() :
-    print("\n\nProjet d'Alexandra BENAMAR et Robin DROIT\nCe programme permet de parser un fichier .pdb\net calcule les distances globales et locales de la proteine.\nUsage : ./main.py -pdb <nom du fichier PDB>\n")
